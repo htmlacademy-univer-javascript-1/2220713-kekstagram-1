62bc7cb8f579517c6b9d3bc4d6b4aa073dae30e2
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2022-09-19 23:11:49 +0300

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+function randomRangeNum(start, end) {
+  if (start >= end) {
+    return Math.floor(Math.random() * (start - end)) + end;
+  }
+  return Math.floor(Math.random() * (end - start)) + start;
+}
 
+
+function maxLenghStr(currentStr, maxLengh) {
+  if (currentStr.lenght <= maxLengh) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+randomRangeNum(20, 30);
+maxLenghStr('2000', 2);
