6e86fc34a65d20a16a0dc430a8d9822883380179
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2022-11-27 23:34:35 +0300

    modified:   index.html 	modified:   js/data.js 	modified:   js/main.js 	new file:   js/renderBigModel.js 	modified:   js/renderingModels.js

--- js/renderingModels.js
+++ js/renderingModels.js
@@ -2,16 +2,16 @@ export const renderModel = function(data) {
   const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
   const imgList = document.createDocumentFragment();
-
-  data.forEach(({url, likes, comments}) => {
-    const imageElement = imageTemplate.cloneNode(true);
-    imageElement.querySelector('.picture__img').src = url;
-    imageElement.querySelector('.picture__likes').textContent = likes;
-    imageElement.querySelector('picture__comments').textContent = comments;
-    imgList.appendChild(imageElement);
-  });
+  const generateElem = () => {
+    for (let i = 0; i < data.length; i++) {
+      const imageElement = imageTemplate.cloneNode(true);
+      imageElement.querySelector('.picture__img').src = data[i].url;
+      imageElement.querySelector('.picture__likes').textContent = data[i].likes;
+      imageElement.querySelector('.picture__comments').textContent = data[i].comments.length;
+      imgList.appendChild(imageElement);
+    }
+  };
+  generateElem();
 
   document.querySelector('.pictures').appendChild(imgList);
 };
-
-renderModel();
