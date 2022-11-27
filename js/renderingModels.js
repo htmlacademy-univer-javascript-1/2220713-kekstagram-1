a22a8787aa1b55b1c4cae56144d89f21d754426a
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2022-11-28 01:25:23 +0300

    modified:   index.html 	modified:   js/renderBigModel.js 	modified:   js/renderingModels.js 	modified:   js/util.js

--- js/renderingModels.js
+++ js/renderingModels.js
@@ -1,4 +1,6 @@
-export const renderModel = function(data) {
+import { showBigModel } from './renderBigModel.js';
+
+export const renderModel = (data) => {
   const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
   const imgList = document.createDocumentFragment();
@@ -8,6 +10,10 @@ export const renderModel = function(data) {
       imageElement.querySelector('.picture__img').src = data[i].url;
       imageElement.querySelector('.picture__likes').textContent = data[i].likes;
       imageElement.querySelector('.picture__comments').textContent = data[i].comments.length;
+      imageElement.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        showBigModel(data[i]);
+      });
       imgList.appendChild(imageElement);
     }
   };
