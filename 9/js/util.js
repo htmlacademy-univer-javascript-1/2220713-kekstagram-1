ae7cea3632c2a5c490b4d7a7317e35c8a2035f11
Keks <keks@htmlacademy.ru>
2022-11-28 01:29:00 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/util.js
@@ -0,0 +1,12 @@
+export const getRandomPositiveInteger = (start, end) => {
+  if (start === end) {
+    return start;
+  }
+  if (start > end) {
+    return Math.floor(Math.random() * (start - end + 1)) + end;
+  }
+  return Math.floor(Math.random() * (end - start + 1)) + start;
+}
+
+// eslint-disable-next-line no-unused-vars
+const checkStringLength = (currentStr, maxLength) => currentStr <= maxLength;
