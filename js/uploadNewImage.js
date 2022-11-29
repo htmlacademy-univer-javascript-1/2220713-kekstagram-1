db22af860109cdc6d90c4527511c93f73b78609b
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2022-11-29 20:22:43 +0300

    modified:   index.html 	new file:   js/uploadImgValidation.js 	new file:   js/uploadNewImage.js 	modified:   js/util.js

---
+++ js/uploadNewImage.js
@@ -0,0 +1,40 @@
+import {form, hashtag, comment, validateForm} from './uploadImgValidation.js';
+
+const uploadFile = document.querySelector('#upload-file');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const buttonClose = document.querySelector('#upload-cancel');
+
+const closeWindow = () => {
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  uploadFile.value = '';
+  hashtag.value = '';
+  comment.value = '';
+};
+
+const closeWindowWithEsc = (evt) => {
+  if (evt.code === 'Escape' && !(evt.target.matches('input')) && !(evt.target.matches('textarea'))) {
+    evt.preventDefault();
+    closeWindow();
+    document.removeEventListener('keydown', closeWindowWithEsc);
+  }
+};
+
+buttonClose.addEventListener('click', () => {
+  closeWindow();
+});
+
+uploadFile.addEventListener('change', () => {
+  imgUploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', (evt) => {
+    closeWindowWithEsc(evt);
+  });
+});
+
+form.addEventListener('submit', (evt) => {
+  if (!validateForm()) {
+    evt.preventDefault();
+  }
+});
