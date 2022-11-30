e15195b87f92f95f67f99a0034a17cee481c0380
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2022-11-30 23:35:45 +0300

    modified:   js/renderBigModel.js

--- js/renderBigModel.js
+++ js/renderBigModel.js
@@ -8,6 +8,63 @@ const body = document.querySelector('body');
 let balanceСounter = 0;
 let index = 5;
 let currentIndex = 0;
+let arrOfComments = [];
+
+const createCommentForList = (comment) => {
+  const listElem = document.createElement('li');
+  listElem.classList.add('social__comment');
+
+  const img = document.createElement('img');
+  img.classList.add('social__picture');
+  img.src = comment.avatar;
+  img.alt = comment.name;
+  img.width = '35';
+  img.height = '35';
+
+  const p = document.createElement('p');
+  p.classList.add('social__text');
+  p.textContent = comment.message;
+
+  listElem.appendChild(img);
+  listElem.appendChild(p);
+  return listElem;
+};
+
+const createCommentsList = () => {
+  const comments = arrOfComments;
+  if (index === comments.length) {
+    loadComments.disabled = true;
+    return;
+  }
+
+  if (comments.length <= FIRST_FIVE_VALUE && balanceСounter === 0) {
+    currentIndex = 0;
+    index = comments.length;
+    firstcountComments.textContent = index;
+  }
+
+  if (balanceСounter > 0 && balanceСounter - FIRST_FIVE_VALUE >= 0) {
+    index += FIRST_FIVE_VALUE;
+    firstcountComments.textContent = index;
+    currentIndex += FIRST_FIVE_VALUE;
+  }
+
+  if (balanceСounter > 0 && balanceСounter - FIRST_FIVE_VALUE <= 0){
+    currentIndex = index;
+    index = comments.length;
+    firstcountComments.textContent = index;
+  }
+
+  balanceСounter = comments.length - firstcountComments.textContent;
+  for (let i = currentIndex; i < index; i++) {
+    const listElem = createCommentForList(comments[i]);
+    commentsList.appendChild(listElem);
+  }
+};
+
+const onClickComments = () => {
+  createCommentsList();
+};
 
 const closeWindow = () => {
   mainElem.classList.add('hidden');
@@ -19,7 +76,9 @@ const closeWindow = () => {
   balanceСounter = 0;
   index = 5;
   currentIndex = 0;
+  arrOfComments = [];
   loadComments.disabled = false;
+  loadComments.removeEventListener('click', onClickComments);
   for (const prevComment of prevComments) {
     prevComment.remove();
   }
@@ -33,60 +92,22 @@ const closeWindowWithEsc = (evt) => {
   }
 };
 
-const createCommentsList = (comments) => {
-  if (index === comments.length) {
-    loadComments.disabled = true;
-    return;
-  }
-  if (balanceСounter > 0 && balanceСounter - FIRST_FIVE_VALUE >= 0) {
-    index += FIRST_FIVE_VALUE;
-    firstcountComments.textContent = index;
-    currentIndex += FIRST_FIVE_VALUE;
-  }
-  else if (balanceСounter > 0 && balanceСounter - FIRST_FIVE_VALUE <= 0){
-    currentIndex = index;
-    index = comments.length;
-    firstcountComments.textContent = index;
-  }
-  balanceСounter = comments.length - firstcountComments.textContent;
-  for (let i = currentIndex; i < index; i++) {
-    const listElem = document.createElement('li');
-    listElem.classList.add('social__comment');
-
-    const img = document.createElement('img');
-    img.classList.add('social__picture');
-    img.src = comments[i].avatar;
-    img.alt = comments[i].name;
-    img.width = '35';
-    img.height = '35';
-
-    const p = document.createElement('p');
-    p.classList.add('social__text');
-    p.textContent = comments[i].message;
-
-    listElem.appendChild(img);
-    listElem.appendChild(p);
-    commentsList.appendChild(listElem);
-  }
-};
-
 const changeDataOfPhoto = (imgElem) => {
   document.querySelector('.big-picture__img').querySelector('img').src = imgElem.url;
   document.querySelector('.likes-count').textContent = imgElem.likes;
   document.querySelector('.social__caption').textContent = imgElem.descriptions;
   document.querySelector('.comments-count').textContent = imgElem.comments.length;
-  createCommentsList(imgElem.comments);
+  createCommentsList();
 };
 
 export const showBigModel = (data) => {
   mainElem.classList.remove('hidden');
   body.classList.add('modal-open');
+  arrOfComments = data.comments;
   changeDataOfPhoto(data);
   document.querySelector('#picture-cancel').addEventListener('click', () => {
     closeWindow();
   });
   document.addEventListener('keydown', closeWindowWithEsc);
-  loadComments.addEventListener('click', () => {
-    createCommentsList(data.comments);
-  });
+  loadComments.addEventListener('click', onClickComments);
 };
